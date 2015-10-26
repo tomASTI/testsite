c19b1a9c30c4692f2b98b3ef88bc7912a3a623bf
Tom Anderson <tom@Toms-MacBook-Pro.local>
2015-10-26 13:38:09 -0700

    test

---
+++ client.js
@@ -0,0 +1,11 @@
+var sitemap = sm.createSitemap ({
+  hostname: 'http://example.com',
+  cacheTime: 600000
+});
+
+sitemap.add({url: '/page-1/'});
+sitemap.add({url: '/page-2/', changefreq: 'monthly', priority: 0.7});
+sitemap.del({url: '/page-2/'});
+sitemap.del('/page-1/');
+
+console.log('hello');
